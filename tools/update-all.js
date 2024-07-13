3ca16f62a7d718b256983ff628095e901559044f
Alexander Danilov <git@modos189.ru>
2024-07-13 11:34:36 +0500

    Force to update readme and meta.json if action is called manually

--- tools/update-all.js
+++ tools/update-all.js
@@ -2,7 +2,7 @@ import {render_readme, make_plugins_json, run_update} from './actions.js';
 import {get_all_metadata_files} from './helpers.js';
 
 const metadata_files = get_all_metadata_files();
-if (await run_update(metadata_files)) {
+if (await run_update(metadata_files) || process.env.FORCE_RENDER !== undefined) {
     render_readme().then();
     make_plugins_json().then();
 }

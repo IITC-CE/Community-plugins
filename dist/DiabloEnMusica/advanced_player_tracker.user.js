bffa9fca9a56bfe98aed0f17361234a49fa1466a
David Iacoviello <diacovmusic@gmail.com>
2025-05-21 18:50:32 -0400

    Update advanced_player_tracker.user.js

--- dist/DiabloEnMusica/advanced_player_tracker.user.js
+++ dist/DiabloEnMusica/advanced_player_tracker.user.js
@@ -6,8 +6,8 @@
 // @version        0.0.4
 // @namespace      https://tempuri.org/iitc/advanced-player-tracker
 // @description    Advanced version of Player Tracker +
-// @updateURL      https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/BlancLapin/advanced_player_tracker.meta.js
-// @downloadURL    https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/BlancLapin/advanced_player_tracker.user.js
+// @updateURL      https://raw.githubusercontent.com/diacoviello/Community-plugins/master/dist/DiabloEnMusica/advanced_player_tracker.meta.js
+// @downloadURL    https://raw.githubusercontent.com/diacoviello/Community-plugins/master/dist/DiabloEnMusica/advanced_player_tracker.user.js
 // @depends        player-activity-tracker@breunigs
 // @include        http://www.ingress.com/intel*
 // @include        https://www.ingress.com/intel*
